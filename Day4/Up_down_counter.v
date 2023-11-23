80f2667c8fba7fff42716bf8e0d0704addbc25dc
ShubhamRaut9922 <143434280+ShubhamRaut9922@users.noreply.github.com>
2023-11-23 18:47:35 +0530

    Create Up_down_counter.v

---
+++ Day4/Up_down_counter.v
@@ -0,0 +1,14 @@
+module up_down_counter(mode,clr,ld,d_in,clk,count);
+input mode,clr,ld,clk;
+input [0:7] d_in;
+output reg [0:7] count;
+always@ (posedge clk)
+if(ld)
+count <= d_in;
+else if (clr)
+count <= 0;
+else if (mode)
+count <= count+1;
+else
+count <= count-1;
+endmodule
